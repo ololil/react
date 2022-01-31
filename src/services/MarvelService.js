f3ee1033c82e0954a589ecb954191b424000272e
Olololi <fomenkooleg97@gmail.com>
2022-01-31 11:57:47 +0200

    Errors

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,37 +4,36 @@ class MarvelService {
     _apiKey = 'apikey=4b577f31b97bbf065669ddea285ffa30';
 
     getResource = async (url) => {
-
         let res = await fetch(url);
-
+    
         if (!res.ok) {
-            throw new Error(`Cloud not tech ${url}, statys: ${res.status}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
     getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-       return res.data.results.map(this._transforCharacter);
-
+        return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transforCharacter(res.data.results[0]);
+        return this._transformCharacter(res.data.results[0]);
     }
 
-    _transforCharacter = (char) => {
+    _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
-
 }
 
 export default MarvelService;
